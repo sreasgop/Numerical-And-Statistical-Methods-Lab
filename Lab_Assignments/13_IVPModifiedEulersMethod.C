a8c52517d395406c4832fd046f898e913ef6af0d
CSGS <148139834+sreasgop@users.noreply.github.com>
2025-04-28 20:20:36 +0530

    Formatting Improved

--- Lab_Assignments/13_IVPModifiedEulersMethod.C
+++ Lab_Assignments/13_IVPModifiedEulersMethod.C
@@ -20,15 +20,15 @@ int main(){
     step:
         x=x0+h; 
         if(x<=xn){
-        yp=y0+h*f(x0,y0);
-        yc=y0+(h/2.0)*(f(x0,y0)+f(x,yp));
-        while(fabs(yp-yc)>tol){
-            yp=yc;
+            yp=y0+h*f(x0,y0);
             yc=y0+(h/2.0)*(f(x0,y0)+f(x,yp));
-        }
-        x0=x;
-        y0=yc;
-        goto step;
+            while(fabs(yp-yc)>tol){
+                yp=yc;
+                yc=y0+(h/2.0)*(f(x0,y0)+f(x,yp));
+            }
+            x0=x;
+            y0=yc;
+            goto step;
         }
 
     printf("By modified Euler's method, y(%.1f)=%.4f",xn,yc);
